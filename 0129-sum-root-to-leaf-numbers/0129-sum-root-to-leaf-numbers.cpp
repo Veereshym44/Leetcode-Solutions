f1e49fb0bd61feeb8455fdbfa207069b500362c7
Veereshym44 <106478720+Veereshym44@users.noreply.github.com>
2023-09-02 01:03:38 +0530

    Time: 4 ms (29.12%), Space: 9.2 MB (63.38%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,28 +11,26 @@
  */
 class Solution {
 public:
-    void ans(TreeNode *root,vector<int>&vec,int cur)
+    int sum=0;
+    void ans(TreeNode *root,int cur)
     {
         if(root==NULL)return;
         if(root->left==NULL && root->right==NULL){
-            vec.push_back(cur*10+root->val);
+            sum+=cur*10+root->val;
             return;
         }
         int x=cur*10+root->val;
-        ans(root->left,vec,x);
-        ans(root->right,vec,x);
+        ans(root->left,x);
+        ans(root->right,x);
         
     }
     int sumNumbers(TreeNode* root) {
         vector<int>vec;
         if(root==NULL )return 0;
         
-        ans(root,vec,0);
-        // for(auto i:vec)
-        // {
-        //     cout<<i<<"\n";
-        // }
-        int sum=accumulate(vec.begin(),vec.end(),0);
-        return sum;
+        ans(root,0);
+      return sum;
+        
+    
     }
 };
