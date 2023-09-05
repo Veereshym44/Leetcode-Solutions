3ce0693948f9d8775829c4dae3936bef483ceed7
Veereshym44 <106478720+Veereshym44@users.noreply.github.com>
2023-09-05 22:20:08 +0530

    Time: 9 ms (36.32%), Space: 11.1 MB (88.34%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -2,21 +2,56 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
       if(head==NULL)return NULL;
-      unordered_map<Node*,Node*>mp;
-      Node* temp=head;
-      while(temp)
-      {
-          mp[temp]=new Node(temp->val);
-          temp=temp->next;
-      }
-      temp=head;
-      while(temp)
-      {
-          mp[temp]->next=mp[temp->next];
-          mp[temp]->random=mp[temp->random];
-          temp=temp->next;
-      }
-      temp=mp[head];
-      return temp;
+    //   unordered_map<Node*,Node*>mp;
+    //   Node* temp=head;
+    //   while(temp)
+    //   {
+    //       mp[temp]=new Node(temp->val);
+    //       temp=temp->next;
+    //   }
+    //   temp=head;
+    //   while(temp)
+    //   {
+    //       mp[temp]->next=mp[temp->next];
+    //       mp[temp]->random=mp[temp->random];
+    //       temp=temp->next;
+    //   }
+    //   temp=mp[head];
+    //   return temp;
+    Node*temp=head;
+    while(temp)
+    {
+        Node* node=new Node(temp->val);
+        node->next=temp->next;
+        temp->next=node;
+        temp=node->next;
+    }
+    temp=head;
+    Node* newHead=head->next;
+    while(temp)
+    {
+        if(temp->random!=NULL)
+        temp->next->random=temp->random->next;
+        else
+        temp->next->random=NULL;
+
+        temp=temp->next->next;
+
+    }
+    temp=head;
+    Node* ans;
+    while(temp)
+    {
+      ans=temp->next;
+      temp->next=ans->next;
+      temp=temp->next;
+      if(temp)
+      ans->next=temp->next;
+      else
+      ans->next=NULL;
+    }
+    return newHead;
+
+
     }
 };
