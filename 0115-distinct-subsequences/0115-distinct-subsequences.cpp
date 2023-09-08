f4eb128079b043d3a0e2b4ca492e5e69c8f458df
Veeresh Kumar Y M <106478720+Veereshym44@users.noreply.github.com>
2023-09-09 00:17:25 +0530

    Time: 34 ms (67.16%), Space: 30.1 MB (28.04%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
-    int ans(string s,string t,int i,int j,vector<vector<int>>&dp)
-    {
-        if(j==t.size())return 1;
+    // int ans(string s,string t,int i,int j,vector<vector<int>>&dp)
+    // {
+    //     if(j==t.size())return 1;
 
-        if(i==s.size())return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s[i]==t[j])
-        {
-            return dp[i][j]=ans(s,t,i+1,j+1,dp)+ans(s,t,i+1,j,dp);
-        }
-        return dp[i][j]=ans(s,t,i+1,j,dp);
-    }
+    //     if(i==s.size())return 0;
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     if(s[i]==t[j])
+    //     {
+    //         return dp[i][j]=ans(s,t,i+1,j+1,dp)+ans(s,t,i+1,j,dp);
+    //     }
+    //     return dp[i][j]=ans(s,t,i+1,j,dp);
+    // }
     int numDistinct(string s, string t) {
-        vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
-        return ans(s,t,0,0,dp);
+       vector<vector<long long>> dp(s.size() + 1, vector<long long>(t.size() + 1, 0));
+    int n=1e9+7;
+        for(int i=0;i<=s.length();i++)
+        {
+            dp[i][t.length()]=1;
+        }
+
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            for(int j=t.size()-1;j>=0;j--)
+            {
+                if(s[i]==t[j])
+                {
+                    dp[i][j]=dp[i+1][j]%n+dp[i+1][j+1]%n;
+                }
+                else{
+                    dp[i][j]=dp[i+1][j]%n;
+                }
+            }
+        }
+        return dp[0][0]%n;
     }
 };
