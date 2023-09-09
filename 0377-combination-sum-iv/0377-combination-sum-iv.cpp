04803ff263de51f1604fba24a6eb8a624f4d3bf0
Veeresh Kumar Y M <106478720+Veereshym44@users.noreply.github.com>
2023-09-09 21:04:17 +0530

    Time: 0 ms (100%), Space: 6.6 MB (85.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    int ans(vector<int>&nums,int target,vector<int>&dp)
-    {
-        if(target==0)return 1;
+    // int ans(vector<int>&nums,int target,vector<int>&dp)
+    // {
+    //     if(target==0)return 1;
 
         
-        if(dp[target]!=-1)return dp[target];
+    //     if(dp[target]!=-1)return dp[target];
 
-        int answer=0;
+    //     int answer=0;
 
-        for(int i=0;i<nums.size();i++)
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         if(nums[i]<=target)
+    //         answer+=ans(nums,target-nums[i],dp);
+    //     }
+
+    //     return dp[target]=answer;
+    // }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int>dp(target+1,0);
+
+        dp[0]=1;
+
+        for(int i=1;i<=target;i++)
         {
-            if(nums[i]<=target)
-            answer+=ans(nums,target-nums[i],dp);
+            for(auto num:nums)
+            {
+                if(num<=i)
+                {
+                    dp[i]+=dp[i-num];
+                }
+            }
         }
 
-        return dp[target]=answer;
-    }
-    int combinationSum4(vector<int>& nums, int target) {
-        vector<int>dp(target+1,-1);
-        return ans(nums,target,dp);
+        return dp[target];
+
     }
 };
