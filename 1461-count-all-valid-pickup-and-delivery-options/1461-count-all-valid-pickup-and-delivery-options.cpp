8f24ac7595c34870f0aca6cb57d1992a0ad7aeb5
Veeresh Kumar Y M <106478720+Veereshym44@users.noreply.github.com>
2023-09-10 10:47:41 +0530

    Time: 35 ms (11.7%), Space: 30.7 MB (5.48%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
+    int mod=1e9+7;
+
+    long long int ans(int pick,int drop,vector<vector<long long int>>&dp)
+    {
+        if(pick==0 && drop==0)return 1;
+        if(pick<0 || drop<0||drop<pick)return 0;
+        if(dp[pick][drop]!=-1)return dp[pick][drop];
+
+      long long int answer=0;
+
+      answer+=pick*ans(pick-1,drop,dp);
+      answer%=mod;
+      answer+=(drop-pick)*ans(pick,drop-1,dp);
+      answer%=mod;
+      return dp[pick][drop]=answer;
+
+    }
     int countOrders(int n) {
-        long long int mod = 1e9 + 7;
-        vector<vector<long long int>> dp(n + 1, vector<long long int>(n + 1, 0));
-
-        // Initialize base cases
-        dp[0][0] = 1;
-
-        for (int drop = 1; drop <= n; drop++) {
-            for (int pick = 0; pick <= drop; pick++) {
-                dp[pick][drop] %= mod;
-                if (pick > 0) {
-                    dp[pick][drop] += pick * dp[pick - 1][drop];
-                    dp[pick][drop] %= mod;
-                }
-
-              
-                if (pick < drop) {
-                    dp[pick][drop] += (drop - pick) * dp[pick][drop - 1];
-                    dp[pick][drop] %= mod;
-                }
-            }
-        }
-
-        return dp[n][n];
+        vector<vector<long long int>>dp(n+1,vector<long long int>(n+1,-1));
+        return ans(n,n,dp);
     }
 };
