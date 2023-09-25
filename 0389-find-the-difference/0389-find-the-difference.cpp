749e10f196bf8e7f88e7f9f64f27e40768e8ba15
Veeresh Kumar Y M <106478720+Veereshym44@users.noreply.github.com>
2023-09-25 18:31:41 +0530

    Time: 4 ms (48.11%), Space: 7.3 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
-
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-
-        int i=0,j=0;
-
-        while(j<s.length() && i<t.length())
-        {
-            if(s[j]!=t[i])
-            return t[i];
-
-            i++;
-            j++;
+    char findTheDifference(std::string s, std::string t) {
+        char result = 0;
+        for (char c : s + t) {
+            result ^= c;
         }
-
-        return t[i];
-        
+        return result;
     }
 };
