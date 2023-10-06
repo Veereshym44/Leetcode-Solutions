8e29e4a0595f30c2e56d1f4a73e6947dc9b22626
Veeresh Kumar Y M <106478720+Veereshym44@users.noreply.github.com>
2023-10-06 22:20:53 +0530

    Time: 1238 ms (9.72%), Space: 129.7 MB (96.23%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,31 +1,39 @@
 class WordDictionary {
 public:
-    vector<string> v;
+        unordered_map<int,vector<string>>mp;
     WordDictionary() {
-        
+
     }
     
     void addWord(string word) {
-        v.push_back(word);
+    mp[word.length()].push_back(word);
+
+        
     }
     
     bool search(string word) {
-        int count=0;
-        int n=word.size();
-        for(int i=0; i<v.size(); i++){
-            if(n==v[i].size()){
-                for(int j=0; j<n; j++){
-                    if(v[i][j]==word[j]||word[j]=='.'){
-                        count++;
-                    }
-                    else{
-                        count=0;
-                        break;
-                    }
-                }
-                if(count==n) return true;
+        int len=word.size();
+        for(auto &temp:mp[len])
+        {
+            int i=0;
+
+            for(i=0;i<len;i++)
+            {
+                if(word[i]=='.')continue;
+                if(word[i]!=temp[i])
+                break;
             }
+
+            if(i==len)return true;
         }
-        return false;
+        
+                return false;
     }
 };
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
