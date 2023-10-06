bfd01580a09e4d49458bf9d2296983d7e71959fc
Veeresh Kumar Y M <106478720+Veereshym44@users.noreply.github.com>
2023-10-06 23:21:29 +0530

    Time: 173 ms (12.68%), Space: 80.1 MB (9.83%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& p, vector<int>& t) {
-        sort(p.begin(),p.end());
-        sort(t.begin(),t.end());
+        // sort(p.begin(),p.end());
+        // sort(t.begin(),t.end());
 
-        int i=0,j=0;
+        // int i=0,j=0;
+        // int ans=0;
+        // while(i<p.size() && j<t.size())
+        // {
+        //     if(p[i]<=t[j])
+        //     {
+        //         ans++;
+        //         i++;
+        //         j++;
+        //     }
+
+        //     else j++;
+
+        // }
+
         int ans=0;
-        while(i<p.size() && j<t.size())
+        priority_queue<int,vector<int>,greater<int>>pq1(p.begin(),p.end()),pq2(t.begin(),t.end());
+        while(!pq1.empty() && !pq2.empty())
         {
-            if(p[i]<=t[j])
+            if(pq1.top()<=pq2.top())
             {
                 ans++;
-                i++;
-                j++;
+                pq1.pop();
+                pq2.pop();
             }
 
-            else j++;
-
+            else pq2.pop();
         }
+
+
 
         return ans;
     }
