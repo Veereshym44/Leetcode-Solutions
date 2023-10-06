daf8e09d1cae8d9181b35c006ced5a8610adab65
Veeresh Kumar Y M <106478720+Veereshym44@users.noreply.github.com>
2023-10-06 13:17:44 +0530

    Time: 2 ms (49.96%), Space: 6.6 MB (22.36%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -6,24 +6,30 @@ using namespace std;
 
 class Solution {
 public:
-    int ans(int n, int val, int k) {
+    int ans(int n, int val, int k,vector<int>&dp) {
         if (n == 0 && k >= 2) return 1;  
-        if (n == 0 && k < 2 ||val>9) return 1;  
+        if (n == 0 && k < 2 ||val>9) return 0; 
+
+        if(dp[n]!=-1)return dp[n]; 
 
         int left = 1;
         if (n - val >= 0) {
-            left = val * ans(n - val, val, k + 1);
+            left = val * ans(n - val, val, k + 1,dp);
         }
 
-        int right = ans(n, val + 1, k);
+        int right = ans(n, val + 1, k,dp);
 
-        return max(left, right);
+        return dp[n]=max(left, right);
     }
 
     int integerBreak(int n) {
         if (n == 2) return 1;
-        if(n==3)return 2;
-        return ans(n, 1, 0);
+        // if(n==3)return 2;
+
+        vector<int>dp(n+1,-1);
+
+        
+        return ans(n, 1, 0,dp);
     }
 };
 
