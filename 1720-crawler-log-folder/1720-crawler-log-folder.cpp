55dd5e832f9343b85d3c0c73f8c36ae07bfae03c
Veeresh Kumar Y M <106478720+Veereshym44@users.noreply.github.com>
2024-07-10 11:56:09 +0530

    Time: 5 ms (46.3%), Space: 14 MB (61.11%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+
+        int cnt=0;
+
+        for(auto i:logs)
+        {
+            if(i=="../")
+            {
+                cnt=max(0,--cnt);
+
+            }
+
+            else if(i=="./")continue;
+            else cnt++;
+        }
+        return cnt;
+    }
+};
